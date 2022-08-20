40c366fe8f816b7e60a03600e928651868bbeb90
Anton <dragon321001@gmail.com>
2022-08-20 14:48:22 +0300

    Completed task with triangle

--- Cycles,Operators/Set_Tasks_2/js/script.js
+++ Cycles,Operators/Set_Tasks_2/js/script.js
@@ -2,12 +2,12 @@
 // Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд.
 
 
-const arr = [3, 5, 8, 16, 20, 23, 50];
-const result = [];
-for (let i=0; i<arr.length; i++) {
-    result[i]=arr[i];
-}
-console.log(result);
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+// for (let i=0; i<arr.length; i++) {
+//     result[i]=arr[i];
+// }
+// console.log(result);
 
 
 
@@ -21,15 +21,15 @@ console.log(result);
 // Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
 
 
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-for(let i=0;i<data.length; i++) {
-    if (typeof(data[i]) == 'number') {
-        data[i]=data[i]*2;
-    } else {
-        data[i] = data[i]+' - done';
-    }
-}
-console.log(data);
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// for(let i=0;i<data.length; i++) {
+//     if (typeof(data[i]) == 'number') {
+//         data[i]=data[i]*2;
+//     } else {
+//         data[i] = data[i]+' - done';
+//     }
+// }
+// console.log(data);
 
 
 
@@ -40,9 +40,37 @@ console.log(data);
 // Разверните массив data наоборот при помощи цикла и запишите данные в массив result.
 // Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
 
-const data = [5, 10, 'Shopping', 20, 'Homework'];
-const result = [];
-for (let i=0; i<data.length; i++) {
-    result[i]=data[data.length-1-i]; 
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+// for (let i=0; i<data.length; i++) {
+//     result[i]=data[data.length-1-i]; 
+// }
+// console.log (result);
+
+
+
+// Задача. Построить фигуру
+//      *         6 пробелов и 1 *
+//     ***        5 пробелов и 3 *
+//    *****       4 пробела и 5 *
+//   *******
+//  *********
+// ***********
+
+const lines = 5;
+let spaces='';
+let result = '';
+for (let i=0; i<=lines; i++) {
+    spaces='';
+    for (let j=0; j<lines-i; j++) {
+        spaces+=" ";
+    }
+    result += spaces;
+    for (let k=0;k<2*i+1;k++) {
+        result+="*";
+    }
+    result+="\n";
 }
-console.log (result);
+console.log(result);
+
+
