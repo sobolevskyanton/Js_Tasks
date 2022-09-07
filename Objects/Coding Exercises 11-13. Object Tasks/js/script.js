66474996e6fa0c90e2cb3b56c98ad85504efdcb9
Anton <dragon321001@gmail.com>
2022-09-07 12:06:49 +0300

    Coding Exercises 15

--- Objects/Coding Exercises 11-13. Object Tasks/js/script.js
+++ Objects/Coding Exercises 11-13. Object Tasks/js/script.js
@@ -145,3 +145,172 @@ isBudgetEnough(shoppingMallData);
 
 
 
+// Задача:
+
+// У вас есть список учеников, которые хотят поиграть в игру:
+
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+// Но команд может быть только 3 по 3 человека. Напишите функцию sortStudentsByGroups, которая принимает в себя массив строк.
+
+// Внутри она сначала сортирует имена по алфавиту. Затем распределяет учеников по 3 человека в 3 группы по алфавитному порядку. Эти группы должны быть массивами. Как итог, функция возвращает новый массив с тремя командами и строкой как 4й элемент.
+
+// Пример:
+
+// sortStudentsByGroups(students)  =>
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: Takesi'
+// ]
+// Если убрать одно студента из списка, то результат будет:
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: -'
+// ]
+// А если добавить одного, то:
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: Takesi, Somebody'
+// ]
+
+
+//Попытки сделать без фиксированной длины первоначального массива
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+// function sortStudentsByGroups(arr) {
+// arr.sort();
+// //console.log(arr);
+// let result=[];
+// let group =[];
+// console.log(Math.floor(arr.length/3));
+// //Math.floor - в меньшую сторону
+// //Math.ceil - в большую сторону
+// for (let i=0; i<Math.ceil(arr.length/3); i++ ) {
+//     group=[];
+// for (let j=i*3; j<(i*3)+3; j++) {
+//     if(i === Math.floor(arr.length/3)) {
+//         if(arr[j] != null) {
+//             group.push(arr[j]);
+//         }
+//     } else {
+//         group.push(arr[j]);
+//     }
+// }
+// result.push(group);
+// }
+// result.push(`Оставшиеся студенты: ${group.join(', ')}`);
+// return result;
+// }
+// console.log(sortStudentsByGroups(students));
+
+
+
+
+
+//
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
+}
+
+sortStudentsByGroups(students);
+
+
+//Замыкание
+function test1() {
+    let test =1;
+    function test2() {
+        return (test = test+1); 
+    }
+    return test2; 
+}
+
+let resultTest1 = test1(); 
+//let resultTest2 = resultTest1();
+console.log(resultTest1());
+console.log(resultTest1());
+console.log(resultTest1()); 
+
+
+
+
+
+
+
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
+
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Закрыто' : answer = 'Открыто';
+
+    return answer;
+}
+
+//console.log(isOpen(openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < +average.slice(0,-1)) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+
+//console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors = [{name: 'Mike', age: 32}];
+    return copy;
+}
+
+console.log(transferWaitors(restorantData));
+console.log(restorantData);
